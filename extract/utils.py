47d79b872b8866ac9263658c4651fa659bac5333
vitoriadaamasceno <maria.damasceno@jusbrasil.com.br>
2025-05-24 14:34:32 -0300

    add: enpoint video

--- extract/utils.py
+++ extract/utils.py
@@ -3,9 +3,9 @@ import re
 
 def extract_id_video(url: str) -> str:
 
-    match = re.search(r"(?:v=|youtu\.be/)([a-zA-Z0-9_-]{11})", url)
+    match = re.search(r"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})", url)
     if not match:
-        raise ValueError("URL inválido")
+        return None
     video_id = match.group(1)
     return video_id
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Body
 from fastapi.responses import JSONResponse
 from extract.pdf_reader import get_pdf_text
+from extract.video_reader import get_video_transcript
+from extract.utils import extract_id_video
 from prompts.t5 import gerar_resumo_t5
 from prompts.gpt import gerar_resumo_gpt
 
@@ -35,4 +37,27 @@ async def summarize_pdf(file: UploadFile = File(...), model="t5"):
     except Exception as e:
         logging.error(f"Erro ao processar o arquivo PDF: {e}")
         return JSONResponse(content={"error": "Erro ao processar o arquivo PDF."}, status_code=500)
+    
+
+@app.post("/summarize/video")
+def summize_video(url : str = Body(...), model="t5"):
+    id_video = extract_id_video(url)
+    if not id_video:
+        logging.error("URL invalida")
+        return JSONResponse(content={"error": "URL inválida"}, status_code=400)
+    try:
+        logging.info(f"ID do vídeo extraído: {id_video}")
+        video_transcript = get_video_transcript(id_video)
+        
+        if model == "gpt":
+            resumo = gerar_resumo_gpt(video_transcript)
+        else:
+            resumo = gerar_resumo_t5(video_transcript)
+        return {"resumo": resumo}
+    except ValueError as e:
+        logging.error(f"Erro ao obter a transcrição do vídeo: {e}")
+        return JSONResponse(content={"error": "Erro ao obter transcrição do vídeo: " + str(e)}, status_code=400)
+    except Exception as e:
+        logging.error(f"Erro ao processar o vídeo: {e}")
+        return JSONResponse(content={"error": "Erro ao processar o vídeo.Erro:" + str(e)}, status_code=500)
     
