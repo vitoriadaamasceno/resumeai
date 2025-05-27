7b39d22138c3493c15ed01aaa1a7d3b1837f2a9d
vitoriadaamasceno <maria.damasceno@jusbrasil.com.br>
2025-05-27 20:27:20 -0300

    fix

--- extract/utils.py
+++ extract/utils.py
@@ -2,6 +2,11 @@ import re
 
 
 def extract_id_video(url: str) -> str | None:
-    match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", url)
-    return match.group(1) if match else None
+
+    match = re.search(r"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})", url)
+    if not match:
+        return None
+    video_id = match.group(1)
+    return video_id
+
 
